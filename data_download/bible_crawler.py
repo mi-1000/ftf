091c22e9dfe3f555bcf6158eb900b3d6fecbba1d
mi-1000 <emilealexandre54@gmail.com>
2024-10-02 10:57:03 +0200

    Updated bible_crawler.py

--- data_download/bible_crawler.py
+++ data_download/bible_crawler.py
@@ -1,11 +1,38 @@
 import asyncio
+import requests
+
+from bs4 import BeautifulSoup
 from crawl4ai import AsyncWebCrawler
+from typing import List
+
+async def crawl_pages(urls: List[str]):
+    res: List[str] = []
+    async with AsyncWebCrawler(verbose=True) as crawler:
+        # for url in urls:
+        url = urls[0]
+        result = await crawler.arun(url=f"http://bibleglot.com{url}", css_selector="div.row")
+        res.append(result.markdown)
+    for i, row in enumerate(res):
+        print(row.split('\n'))
+        # TODO Add to file and update sources.xml
+
+def get_pages_list(url="http://bibleglot.com/pair/Vulgate/FreSegond") -> List[str]:
+    res = []
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        links = soup.find_all('a')
+
+        for link in links:
+            href = link.get('href')
+            if href and href not in ['#', '/'] and href.startswith("/pair/Vulgate/FreSegond/"):
+                res.append(href)
+    return res
 
 async def main():
-    async with AsyncWebCrawler(verbose=True) as crawler:
-        result = await crawler.arun(url="http://bibleglot.com/pair/Vulgate/FreSegond/Gen.1/")
-        print(result.markdown)
-        # TODO Improve coding style, extract text from this page and iterate over all the pages, then output this into data_latin and update sources.xml
+    pages = get_pages_list()
+    await crawl_pages(pages)
 
 if __name__ == "__main__":
     asyncio.run(main())
