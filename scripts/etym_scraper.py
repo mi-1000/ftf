5a6d7b1027e943b6b7c4f86cf4d49cc72b593ff1
mi-1000 <emilealexandre54@gmail.com>
2024-12-09 03:04:01 +0100

    Updated comments

--- scripts/etym_scraper.py
+++ scripts/etym_scraper.py
@@ -5,7 +5,6 @@ import time
 from bs4 import BeautifulSoup
 
 def extract_dates(word):
-    # URL de recherche pour le CNRTL
     url = f"https://www.cnrtl.fr/definition/{word}"
     try:
         response = requests.get(url)
@@ -14,21 +13,20 @@ def extract_dates(word):
         print(f"Erreur lors de la requête pour le mot '{word}': {e}")
         return None
 
-    # Analyse de la page HTML
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # print(soup)
 
-    # Trouve les balises <b> correspondantes
+    # On cherche la section étymologie de la page
     start_tag = soup.find("b", string=re.compile(r"^.*Étym?o?l?\. (et Hist\.)?\s*:?.*$", re.IGNORECASE))
     end_tag = soup.find("b", string=re.compile(r"^.*Fréq\. (abs\.)? (litt\.|littér\.)?.*$", re.IGNORECASE))
     
-    if not start_tag:
+    if not start_tag: # Si on ne trouve pas la section étymologie, on récupère toute la page
         # print(f"Les balises <b> nécessaires ne sont pas présentes pour le mot '{word}'.")
         section = soup.find("div", attrs={"id": "contentbox"}).text
-        section = re.sub(r"<.*>", " ", section)
+        section = re.sub(r"<.*>", " ", section) # On retire les tags HTML
         if section is None:
-            return None
+            return None # Si vraiment on ne trouve rien, on retourne None
     else:
         # Extrait le texte entre les deux balises
         section = ""
@@ -36,15 +34,15 @@ def extract_dates(word):
             if element == end_tag:
                 break
             section += element.get_text(separator=" ") + " "
-    section = re.sub(r"pp?\.\s*\d+(\s*[,;\-\/]\s*\d+)*", "", section) # On retire les numéros de pages qui risquent d'être confondus avec des dates
+    section = re.sub(r"(pp?|pages?)\.+\s*\d+(\s*[,;\-\/]\s*\d+)*", "", section) # On retire les numéros de pages qui risquent d'être confondus avec des dates
     
     # print(section)
 
-    # Extrait les dates (années et siècles)
+    # On extrait les dates (années et siècles)
     date_pattern = re.compile(r"\b(?<!\bp\.\s)(?<!\bp\.)(1[0-9]{3}|20[0-9]{2}|(\d{3}))|((\d{1,2}|[iIvVxX]+)\s*(eme|ème|è|e|ᵉ)\s*(?:s|S)(i(?:è|e)cle)?)\b") # Demande à ChatGPT de t'expliquer ça pcq flemme
     matches = re.findall(date_pattern, section)
 
-    # Convertit les résultats en années et les ajoute à une liste
+    # On convertit les résultats en années et les ajoute à une liste
     dates = []
     for match in matches:
         if type(match) == tuple:
@@ -61,7 +59,7 @@ def extract_dates(word):
     return sorted(dates)
 
 # Exemple d'utilisation avec des mots au pif
-words = ["amour", "rabbin", "ordinateur", "zircon", "dinguerie", "de", "un", "tu", "jour", "ignoble", "voiture", "robot", "abalourdissement", "weltanschauung", "ab hoc et ab hac", "souffrance", "incongru", "désopilant", "néologisme", "acupuncture", "constitution", "paganisme", "fenêtre", "parapluie", "avion"]
+words = ["téléphone", "amour", "rabbin", "ordinateur", "zircon", "dinguerie", "de", "un", "tu", "jour", "ignoble", "voiture", "robot", "abalourdissement", "weltanschauung", "ab hoc et ab hac", "souffrance", "incongru", "désopilant", "néologisme", "acupuncture", "constitution", "paganisme", "fenêtre", "parapluie", "avion"]
 
 if __name__ == "__main__":
     timestamp_init = time.time()
