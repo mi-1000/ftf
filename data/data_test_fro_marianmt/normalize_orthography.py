84af1cbadf4f704fa32a4f8cbba8d1a83425166c
Pierre Tbt <pierre.thiebaut5@etu.univ-lorraine.fr>
2024-12-10 14:50:43 +0100

    normalize

--- data/data_test_fro_marianmt/normalize_orthography.py
+++ data/data_test_fro_marianmt/normalize_orthography.py
@@ -1,17 +1,24 @@
 import cltk
 import nltk
+import os
+
+nltk.download('punkt_tab')
 
 def normalizer(file):
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     for line in lines:
-        words = nltk.tokenize(line)
-        normalize_words = cltk.normalize_fr(words)
+        words = nltk.tokenize.word_tokenize(line)
+        normalize_words = cltk.fro.normalize_fr(words)
 
         line = "".join(normalize_words)
 
     print(lines[:20])
 
 def main():
-    file = "chanson_de_rolland_fro"
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "chanson_de_roland_fro.txt")
+    normalizer(file)
+
+if __name__ == "__main__":
+    main()
